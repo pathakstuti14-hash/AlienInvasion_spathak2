6eedc6b2f9f114d449e67a09cc9247d8624ef7cc
pathakstuti14-hash <pathak.stuti14@gmail.com>
2026-07-27 13:12:05 -0400

    Documnetation

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,3 +1,11 @@
+"""
+Match Coins
+Stuti Pathak
+The Alien Invasion program is a virtual game, similar to Space Invaders, where the ship controlled by the user on the right side of the screen moves vertically to shoot down aliens coming from the left side of the screen towards with bullets from the space ship. This file is the engine of the game Alien Invasio.
+Starter code: From Python Crash Course book and Python Programming lectures
+7/27/2026
+"""
+
 import sys
 import pygame
 
