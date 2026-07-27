c125ebe23f896ba9711d18f1492f062d1bf8cb19
pathakstuti14-hash <pathak.stuti14@gmail.com>
2026-07-27 13:23:16 -0400

    started class from lecture, I copied the documentation from match coins, so I fixed it up

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,8 @@
 """
-Match Coins
+Alien Invasion
 Stuti Pathak
 The Alien Invasion program is a virtual game, similar to Space Invaders, where the ship controlled by the user on the right side of the screen moves vertically to shoot down aliens coming from the left side of the screen towards with bullets from the space ship. This file is the engine of the game Alien Invasio.
-Starter code: From Python Crash Course book and Python Programming lectures
+Starter code: From Python Crash Course book and Python Programming lectures (Unit 6).
 7/27/2026
 """
 
@@ -11,3 +11,11 @@ import pygame
 
 if __name__ == '__main__':
     pass
+
+class AlienInvasion:
+    def __init__(self):
+        pygame.init()
+        self.screen= pygame.display.set_mode(1200, 800)
+        pygame.display.set_caption("Alien Invasion")
+
+    
