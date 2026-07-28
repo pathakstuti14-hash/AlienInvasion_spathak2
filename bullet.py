2055103d08086dedb4943fb875fbf7e2c33251c8
pathakstuti14-hash <pathak.stuti14@gmail.com>
2026-07-28 15:06:22 -0400

    Created Bullet class, with bullets on the top of the ship image

--- bullet.py
+++ bullet.py
@@ -1,1 +1,16 @@
 import pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+    def __init__(self, ai_game):
+        super().__init__()
+        self.screen= ai_game.screen
+        self.settings=ai_game.settings
+        self.color= self.settings.bullet_color
+
+        self.rect= pygame.Rect(0,0, self.settings.bullet_width, self.settings.bullet_height)
+
+        self.rect.midtop = ai_game.ship.rect.midtop
+
+    def update(self):
+        self.rect.x -= self.settings.bullet_speed
