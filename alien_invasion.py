8bb823ab84866d5a1f01e07bec81ac5827325281
pathakstuti14-hash <pathak.stuti14@gmail.com>
2026-08-03 21:52:55 -0400

    alien fleet made!!

--- alien_invasion.py
+++ alien_invasion.py
@@ -96,8 +96,11 @@ class AlienInvasion:
         alien_width = alien.rect.width
         current_x = alien_width
         while current_x <(self.settins.screen_width -2 * alien_width):
+            new_alien = Alien(self)
+            new_alien.rect.x=current_x
+            self.aliens.add(new_alien)
+            current_x+=2* alien_width
             
-        self.aliens.add(alien)
 
 
 
