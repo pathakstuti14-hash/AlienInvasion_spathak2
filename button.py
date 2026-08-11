2c4d5a1426e2fb357d843abb3b4005452d410e8b
pathakstuti14-hash <pathak.stuti14@gmail.com>
2026-08-10 23:28:20 -0400

    Put the message on the display button

--- button.py
+++ button.py
@@ -6,4 +6,19 @@ class Button:
         self.screen_rect= self.screen.get_rect()
 
         self.width, self.height= 200, 50
-        
+        self.button_color= (0, 135, 0)
+        self.text_color= (255, 255, 255)
+        self.font= pygame.font.SysFont(None, 48)
+
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
+        self.rect.center = self.screen_rect.center
+        self._prep_msg()
+
+        self.msg= msg
+    def _prep_msg(self):
+      self.msg_image= self.font.render(self.msg, True, self.text_color, self.button_color)
+      self.msg_image.rect= self.msg_image.get_rect()
+      self.msg_image.rect.center= self.rect.center
+
+      
+
